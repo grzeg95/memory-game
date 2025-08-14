b64f958f9ad2c86869d5a0e17e391fe261f534a3
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-08-14 19:40:23 +0200

    fix: add redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
+import {Provider} from 'react-redux';
 import {App} from './App.tsx'
 import {ModalProvider} from './components/ui/Modal/context/ModalProvider';
+import {store} from './state/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ModalProvider>
-      <App/>
-    </ModalProvider>
+    <Provider store={store}>
+      <ModalProvider>
+        <App/>
+      </ModalProvider>
+    </Provider>
   </StrictMode>,
 )
