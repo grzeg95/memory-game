a910fe310ea159bbeef5e52810b506afed51dd5a
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-08-14 20:23:51 +0200

    feat: adjust App with state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import {useSelector} from 'react-redux';
+import {MainMenu} from './pages/MainMenu';
+import type {RootState} from './state/store';
+
 export function App() {
 
+  const page = useSelector((state: RootState)=> state.pages.page);
+
   return (
-    <main>Main</main>
+    <>
+      {page === 'MainMenu' && <MainMenu/>}
+    </>
   )
 }
