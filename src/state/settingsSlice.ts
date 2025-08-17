e056ccfcbc793ca95f2dffef67ae1451b916e60f
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-08-17 15:20:12 +0200

    fix: settingsSlice rename state interface

--- src/state/settingsSlice.ts
+++ src/state/settingsSlice.ts
@@ -4,27 +4,27 @@ type Theme = 'numbers' | 'icons'
 type NumberOfPlayers = 1 | 2 | 3 | 4;
 type GridSize = 4 | 6;
 
-interface CartState {
+interface SettingsState {
   theme: Theme;
   numberOfPlayers: NumberOfPlayers;
   gridSize: GridSize
 }
 
-const initialState: CartState = {
+const initialState: SettingsState = {
   theme: 'numbers',
   numberOfPlayers: 1,
   gridSize: 4
 }
 
-const setTheme: CaseReducer<CartState, PayloadAction<{theme: Theme}>> = (state, action) => {
+const setTheme: CaseReducer<SettingsState, PayloadAction<{theme: Theme}>> = (state, action) => {
   state.theme = action.payload.theme;
 }
 
-const setNumberOfPlayers: CaseReducer<CartState, PayloadAction<{numberOfPlayers: NumberOfPlayers}>> = (state, action) => {
+const setNumberOfPlayers: CaseReducer<SettingsState, PayloadAction<{numberOfPlayers: NumberOfPlayers}>> = (state, action) => {
   state.numberOfPlayers = action.payload.numberOfPlayers;
 }
 
-const setGridSize: CaseReducer<CartState, PayloadAction<{gridSize: GridSize}>> = (state, action) => {
+const setGridSize: CaseReducer<SettingsState, PayloadAction<{gridSize: GridSize}>> = (state, action) => {
   state.gridSize = action.payload.gridSize;
 }
 
