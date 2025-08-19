1702aa91b8149d3ce718a494742348d0025fd67a
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2025-08-19 16:52:36 +0200

    feat: add PauseModal

---
+++ src/components/modals/PauseModal.tsx
@@ -0,0 +1,56 @@
+import {cva} from 'class-variance-authority';
+import {useDispatch} from 'react-redux';
+import {gameActions} from '../../state/gameSlice';
+import {pagesActions} from '../../state/pagesSlice';
+import type {AppDispatch} from '../../state/store';
+import {Button} from '../ui/Button/Button';
+import {Modal} from '../ui/Modal/Modal';
+
+const modalContentStyles = cva([
+  'flex', 'flex-col', 'gap-[16px]', 'p-[24px]', 'bg-off-white', 'rounded-[10px]', 'max-w-[327px]', 'w-full',
+  'tablet:max-w-[654px]', 'tablet:gap-[25px]'
+]);
+
+type PauseModal = {
+  onClose: (event: number | string | object | null | undefined) => void;
+}
+
+export function PauseModal({onClose}: PauseModal) {
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  function close() {
+    onClose(undefined);
+  }
+
+  function handleResumeGame() {
+    onClose(undefined);
+  }
+
+  function handleNewGame() {
+    dispatch(pagesActions.setPage({page: 'MainMenu'}));
+    close();
+  }
+
+  function handleRestartGame() {
+    dispatch(gameActions.initializeGame());
+    dispatch(gameActions.setTimerState({state: 'restart'}));
+    close();
+  }
+
+  return (
+    <Modal isOpen={true} onClose={onClose}>
+      <div className={modalContentStyles()}>
+        <Button size='big' className='w-full' onClick={() => handleRestartGame()}>
+          Restart
+        </Button>
+        <Button size='big' appearance='secondary' className='w-full' onClick={() => handleNewGame()}>
+          New Game
+        </Button>
+        <Button size='big' appearance='secondary' className='w-full' onClick={() => handleResumeGame()}>
+          Resume Game
+        </Button>
+      </div>
+    </Modal>
+  )
+}
